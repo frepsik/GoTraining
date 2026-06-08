b8e243c588d2811efb6a848974d142a301b34203
Freps <spektor1028@gmail.com>
2026-06-08 17:15:57 +0300

    Добавил обработку HttpQuery

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func defaultHandleFunc(w http.ResponseWriter, r *http.Request) {
+	fooParam := r.URL.Query().Get("foo")
+	booParam := r.URL.Query().Get("boo")
+
+	fmt.Println("foo param:", fooParam)
+	fmt.Println("boo param:", booParam)
+}
+
+func main() {
+	http.HandleFunc("/default", defaultHandleFunc)
+	fmt.Println("Стартуем сервер")
+	if err := http.ListenAndServe(":9091", nil); err != nil {
+		fmt.Println("fail start server: ", err)
+	}
 }
